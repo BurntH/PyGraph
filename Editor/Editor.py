7fb99a1d52721e9d175ea4611101cab0a0214755
BurntH <120578712+BurntH@users.noreply.github.com>
2022-12-17 13:50:39 +0800

    Add files via upload

--- Editor/Editor.py
+++ Editor/Editor.py
@@ -3,8 +3,16 @@ import networkx as nx
 import Core.Force as fs
 import Structure.Vec2 as Vec2
 import Core.DrawNode as drnd
+import Core.DrawEdge as dreg
 import uuid
+
 dt = 1
+
+# default and highlight colors
+node_df = (255,255,255,255)
+edge_df = (255,255,255,255)
+node_hl = (255,0,255,255)
+edge_hl = (255,0,255,255)
 
 
 class Editor:
@@ -43,8 +51,8 @@ class Editor:
                 # If it is not marked as updated
                     # Then we remove the node (from window)
                 # If it is marked as updated
-                    # Reset updated to False and draw it on the screen                  
-
+                    # Reset updated to False and draw it on the screen  
+        
         for node in self.graph:
             if node in self.node_dict:
                 new_vel = Vec2.Vec2(0, 0)
@@ -60,6 +68,7 @@ class Editor:
                 self.node_dict[node]["pos"] = Vec2.Vec2(0, 0)
                 self.node_dict[node]["vel"] = Vec2.Vec2(0, 0)
                 self.node_dict[node]["uuid"] = str((uuid.uuid4()).int)[:8]
+                self.node_dict[node]["color"] = (255,255,255,255)
                 self.node_dict[node]["updated"] = True
                 self.node_dict[node]["created"] = False
                 
@@ -68,22 +77,58 @@ class Editor:
                     self.node_dict.remove(self.graph.nodes[node])
                 else:
                     if self.node_dict[node]["created"]:
-                        drnd.draw_node(self.graph_to_view_coords(self.node_dict[node]["pos"]), self.window, self.node_dict[node]["uuid"], create = False, scale = self.scale)
+                        drnd.draw_node(self.graph_to_view_coords(self.node_dict[node]["pos"]), self.window, self.node_dict[node]["uuid"],fill_color = self.node_dict[node]["color"], create = False, scale = self.scale)
                     else:
-                        drnd.draw_node(self.graph_to_view_coords(self.node_dict[node]["pos"]), self.window, self.node_dict[node]["uuid"], create = True, scale = self.scale)
+                        drnd.draw_node(self.graph_to_view_coords(self.node_dict[node]["pos"]), self.window, self.node_dict[node]["uuid"],fill_color = self.node_dict[node]["color"], create = True, scale = self.scale)
                         self.node_dict[node]["created"] = True
-
+                        
+        #draw edge                 
+        for edge in self.graph.edges:
+            edge = frozenset(edge)
+            self.edge_dict[edge]["ends"][0][1] = self.node_dict[self.edge_dict[edge]["ends"][0][0]]["pos"]
+            self.edge_dict[edge]["ends"][1][1] = self.node_dict[self.edge_dict[edge]["ends"][1][0]]["pos"] 
+            self.edge_dict[edge]["updated"] = True
+            if edge not in self.edge_dict:
+                self.edge_dict[edge] = dict()
+                self.edge_dict[edge]["uuid"] = str((uuid.uuid4()).int)[:8]
+                self.edge_dict[edge]["color"] = (255,255,255,255)
+                self.edge_dict[edge]["created"] = False
+            
+            
+            for edge in self.edge_dict:
+                if not self.edge_dict[edge]["updated"]:
+                    self.edge_dict.remove(self.graph.edges[edge])
+                else: 
+                    if self.edge_dict[edge]["created"]:
+                        dreg.draw_edge(self.graph_to_view_coords(self.edge_dict[edge]["ends"][0][1]), self.graph_to_view_coords(self.edge_dict[edge]["ends"][1][1]), self.window, self.edge_dict[edge]["uuid"], self.edge_dict[edge]["color"], create = False)
+                    else:
+                        dreg.draw_edge(self.graph_to_view_coords(self.edge_dict[edge]["ends"][0][1]), self.graph_to_view_coords(self.edge_dict[edge]["ends"][1][1]), self.window, self.edge_dict[edge]["uuid"], self.edge_dict[edge]["color"], create = True)
+                        self.edge_dict[edge]["created"] = True
+                    
 ### When users edit graph only using mouse and kbd
 
-    def add_node(self, node, pos = [0,0]):
+    def add_node(self, node, pos = [0,0], color = node_df):
         self.graph.add_node(node)
         pos = Vec2.Vec2(pos)
         self.node_dict[node] = dict()
         self.node_dict[node]["pos"] = pos
         self.node_dict[node]["vel"] = Vec2.Vec2(0, 0)
         self.node_dict[node]["uuid"] = str((uuid.uuid4()).int)[:8]
+        self.node_dict[node]["color"] = color
         self.node_dict[node]["updated"] = True
         self.node_dict[node]["created"] = False
+        
+    def add_edge(self, node1, node2, color = edge_df):
+        
+        edge = frozenset({node1,node2})
+        
+        self.graph.add_edge(node1, node2)
+        self.edge_dict[edge] = dict()
+        self.edge_dict[edge]["ends"] = [[node1,self.node_dict[node1]["pos"]], [node2,self.node_dict[node2]["pos"]]]
+        self.edge_dict[edge]["uuid"] = str((uuid.uuid4()).int)[:8]
+        self.edge_dict[edge]["color"] = color
+        self.edge_dict[edge]["updated"] = True
+        self.edge_dict[edge]["created"] = False
 
     # def delete_node(self, node):
     #     self.graph.remove_nodes_from([node])      
@@ -95,8 +140,10 @@ class Editor:
     def set_camera(self, scale: float, offset: list):
         self.scale = scale
         self.offset = Vec2.Vec2(offset)
-
-
         
+    
+
+
+
 
             
