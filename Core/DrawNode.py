e30349035737c20d71cc014e278b3ca31e9d89e0
BurntH <120578712+BurntH@users.noreply.github.com>
2022-12-17 13:49:06 +0800

    Add files via upload

--- Core/DrawNode.py
+++ Core/DrawNode.py
@@ -3,7 +3,7 @@ import dearpygui.dearpygui as dpg
 
 node_radius = 25
 
-def draw_node(pos: Vec2, window: str, tag, scale: float, create: bool = False):
+def draw_node(pos: Vec2, window: str, tag, scale: float, fill_color: tuple =(255,255,255,255), create: bool=False):
     pos_array = pos.to_precision_array(8)
     # print("pos_array", pos_array)
     # print("tag", tag)
@@ -11,5 +11,5 @@ def draw_node(pos: Vec2, window: str, tag, scale: float, create: bool = False):
     if not create:
         dpg.configure_item(tag, center=pos_array)
     else:
-        dpg.draw_circle(pos_array, node_radius * scale, tag=tag, parent= window, fill=(255, 255, 255, 255))
+        dpg.draw_circle(pos_array, node_radius * scale, tag=tag, parent= window, fill=fill_color)
 
